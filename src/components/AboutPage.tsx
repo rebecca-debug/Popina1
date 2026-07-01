3df96133a677d584321bf157487e7587674fbd7d
Rebecca B <rebecca@fantailweddings.com>
2026-07-01 12:40:34 +1200

    Update AboutPage.tsx

--- src/components/AboutPage.tsx
+++ src/components/AboutPage.tsx
@@ -93,7 +93,7 @@ const AboutPage = () => {
           >
             <div className="flex justify-center mb-10">
               <img 
-                src="src="/popina_saffron_logo.jpg?v=8" 
+                src="/popina_saffron_logo.jpg?v=9" 
                 alt="Popina Saffron Logo" 
                 className="h-28 md:h-36 w-auto object-contain"
               />
